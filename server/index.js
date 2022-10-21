6b394ee45af813e89a34afd5046bc827182f5d54
webdevkirill <krill.fyodorov@gmail.com>
2022-10-21 19:44:10 +0300

    test commit 2

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ const randomCar = require('./randomCar');
 const wss = new ws.Server(
 	{
 		port: 5001,
-    
+
 	},
 	() => {
 		console.log('Server start on 5001 port');
 		setInterval(() => {
 			const car = randomCar();
+      
 			sendCar(car);
 			console.log(JSON.stringify(car));
 		}, 5000);
